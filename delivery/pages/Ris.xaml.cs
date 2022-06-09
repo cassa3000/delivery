f5f4f71afedb661e305bdf9ce472e83783ebf240
Ponchik <Ponchik@PONCHIK>
2022-06-09 07:17:43 +0500

    Добавьте файлы проекта.

---
+++ delivery/pages/Ris.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace delivery.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Ris.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+
+            table.ItemsSource = App.Context.MenuRis.ToList();
+        }
+
+        private void Zak_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ delivery/pages/Rol.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace delivery.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Rol.xaml
+    /// </summary>
+    public partial class Rol : Page
+    {
+        public Rol()
+        {
+            InitializeComponent();
+            tableRoll.ItemsSource = App.Context.MenuRoll.ToList();
+        }
+        private void Zak_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
